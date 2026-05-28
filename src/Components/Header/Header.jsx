f231c7f81a19ae1953088a429c6f3da895238673
Corentin Busi <corentinbusi@gmail.com>
2026-05-28 13:33:46 +0200

    Intégration du Header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,34 @@
+import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPowerOff } from "@fortawesome/free-solid-svg-icons";
+
+import Logo from "../../Assets/TAMG-Logo.png";
 import "./header.css";
 
 export function Header() {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    // Ici : logique pour vider le token d'authentification
+    console.log("Déconnexion de l'utilisateur");
+    navigate("/");
+  };
   return (
     <header>
-      <div>This a header</div>
+      <img src={Logo} alt="Logo These Are My Games" className="logoHeader" />
+      <div className="logOut">
+        <button className="logOutButton" onClick={handleLogout}>
+          Déconnexion
+        </button>
+        <FontAwesomeIcon
+          icon={faPowerOff}
+          className="logOutIcon"
+          onClick={handleLogout}
+          role="button"
+          aria-label="Se déconnecter"
+          tabIndex="0"
+        />
+      </div>
     </header>
   );
 }
